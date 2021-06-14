3ce3b64b122818048f127ab7819ba8fdb296d3e0
Muhammad Fauzan Hadi Saputra <fraternityfra@gmail.com>
2021-06-14 07:13:34 +0700

    revisi kedua

--- hello.js
+++ hello.js
@@ -1,2 +1,9 @@
 //hello program
 console.log("hello javascript");
+
+//js variable
+var angka = 12; //var digunakan pada older javascript
+let angka_kedua = 15; //let digunakan pada ES6 / New JS
+//cetak
+console.log(angka);
+console.log(angka_kedua);
